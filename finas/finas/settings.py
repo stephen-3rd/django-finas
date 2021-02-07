effa0a50d309a1bdf30613b357ca337a79fcf4d3
Milinches <ibrogenius5165@gmail.com>
2021-02-07 17:24:28 +0100

    changes made

--- finas/finas/settings.py
+++ finas/finas/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'finatic.apps.FinaticConfig'
+    'finatic.apps.FinaticConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- finas/finatic/models.py
+++ finas/finatic/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+STATUS = (
+    (0, "Drafts"),
+    (1, "Publish")
+)
+
+
+class Lust(models.Model):
+    picture = models.ImageField()
+    description = models.TextField()
+    author = models.ForeignKey(User, on_delete= models.CASCADE())
