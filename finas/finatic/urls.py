bff1a14465161b39ce0113f879e51fd10880356a
milinches <ibrogenius5165@gmail.com>
2021-02-12 16:50:17 +0100

    Django delete View

--- finas/finatic/urls.py
+++ finas/finatic/urls.py
@@ -6,7 +6,9 @@ app_name = 'finatic'
 urlpatterns = [
     path('', views.LustView.as_view(), name="list"),
     path('<slug:slug>/', views.LustDetail.as_view(), name="detail"),
-    path('lustr/add', views.CreateLust.as_view(), name="create")
+    path('lustr/add', views.CreateLust.as_view(), name="create"),
+    path('lustr/<slug:slug>/', views.UpdateLust.as_view(), name="update"),
+    path('lustr/<slug:slug>/delete/', views.DeleteLust.as_view(), name="delete"),
 ]
 
 
